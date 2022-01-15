d78b255a0d07875ac0d84f39a522d3646dd5de42
SCUT-Corgis <512141203@qq.com>
2022-01-15 10:56:32 +0800

    lab2 100次测试通过

--- src/raft/test.sh
+++ src/raft/test.sh
@@ -1,5 +1,8 @@
-count=10
-    for i in $(seq $count); do
-        go test -run 2B >> out.log -race
-        done
+count=100
+    for i in $(seq 1 ${count})
+    do
+        echo "第 ${i} 轮测试" >> out.log 
+        echo "-------------------------------------" >> out.log 
+        go test >> out.log -race
+    done
 
